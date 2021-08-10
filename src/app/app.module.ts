784991c058924bbad0063209509936ba107bb8e2
Bogdan Matvienko <matvienko.bohdan.dev@gmail.com>
2021-08-10 09:41:20 +0300

    pagination implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { JokeListComponent } from './components/joke-list/joke-list.component';
 import { JokeItemComponent } from './components/joke-item/joke-item.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { JokeItemComponent } from './components/joke-item/joke-item.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/joke-list/joke-list.component.ts
+++ src/app/components/joke-list/joke-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {JokesService} from "../../services/jokes.service";
 import {Joke} from "../../interfaces/joke.interface";
+import {PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-joke-list',
@@ -10,13 +11,30 @@ import {Joke} from "../../interfaces/joke.interface";
 export class JokeListComponent implements OnInit {
 
   jokes: Joke[] = [];
+  pageEvent: PageEvent;
+  length: number;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
   constructor(
     private jokeService: JokesService
   ) { }
 
   ngOnInit(): void {
-    this.jokeService.getAll().subscribe(data => this.jokes = data.result);
+    this.jokeService.getAll().subscribe(data => {
+      this.jokes = data.result.slice(0, this.pageSize)
+      this.length = data.result.length
+    });
+  }
+
+  getAllJokes(e: PageEvent) {
+    this.jokeService.getAll().subscribe(data => {
+      const page = e.pageIndex;
+      const start = page * this.pageSize;
+      const end = start + this.pageSize;
+      this.jokes = data.result.slice(start, end)
+    })
+    return this.pageEvent;
   }
 
 }
